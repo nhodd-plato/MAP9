ef5b77a3e886fc97cab5128396c4f37e24b932f8
nhodd-plato <nick.hodd@platotech.com>
2024-04-29 09:57:28 -0300

    test

--- Test Suites/testSuite.ts
+++ Test Suites/testSuite.ts
@@ -5,17 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
+   <numberOfRerun>45</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
+   <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>61e30f84-22b0-48a2-8aa2-b16881a516c6</testSuiteGuid>
    <testCaseLink>
-      <guid>3b9256f1-dc03-4e97-ab55-f951e4e439dd</guid>
+      <guid>b3ac7632-c754-4f8a-8839-346ad9d724b8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/PayGrade/PayGrade_Add/TC_5.2.2.1_Add_PG_002</testCaseId>
+      <testCaseId>Test Cases/EmployeeDetails/EmployeeDetails_Add/TC_6.1.2.1_Add_EMP_002</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
